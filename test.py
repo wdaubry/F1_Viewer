042d1002c7320f1e8132204971c908007d70c89d
wdaubry <daubry.wilson@gmail.com>
2023-04-21 16:39:04 +0200

    Initial test of the F1 API

---
+++ test.py
@@ -0,0 +1,23 @@
+import requests
+from pprint import PrettyPrinter
+
+BASE_URL = "http://ergast.com/api/f1"
+ALL_JSON = "/current/driverStandings.json"
+
+request_url = f"{BASE_URL}{ALL_JSON}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()['MRData']
+else:
+    print("Error")
+
+standing = data['StandingsTable']['StandingsLists'][0]['DriverStandings']
+
+result = {}
+for i in range(len(standing)):
+    classement = {standing[i]['Driver']['familyName']}
+    points = {standing[i]['points']}
+    result[i+1] = (classement, points)
+
+print(result)
